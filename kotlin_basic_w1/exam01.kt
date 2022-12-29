7556e60d481e782e6f91e6f47913696ebe749883
Ahyoung Cho <joayoung03@gmail.com>
2022-12-29 23:41:54 +0900

    change

--- kotlin_basic_w1/exam01.kt
+++ kotlin_basic_w1/exam01.kt
@@ -1,15 +1,13 @@
 package kotlin_basic_w1
 
 fun main() {
-    var var1 : Int = 10
-    var var2 : Float = 10.1f  // 4byte 이용
-    var var3 : Double = 10.2  // float형 보다 정밀도가 높음 - > 8byte 이용
-    var var4 : Char = '안'  // 문자 1개
-    var var5 : String = "안드로이드"  // 여러글자의 문자열
+    var name : String = "슈먼이"
+    var id : Int = 20201231
+    var age : Int = 22
+    var dept : Char = 'S'
 
-    println(var1)
-    println(var2)
-    println(var3)
-    println(var4)
-    println(var5)
+    println(name)
+    println(id)
+    println(age)
+    println(dept)
 }
