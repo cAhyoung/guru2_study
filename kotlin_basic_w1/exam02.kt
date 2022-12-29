16e0c77dc8b7a4cf934d50cd5f14301a172c2545
Ahyoung Cho <joayoung03@gmail.com>
2022-12-30 01:56:36 +0900

    change

--- kotlin_basic_w1/exam02.kt
+++ kotlin_basic_w1/exam02.kt
@@ -1,2 +1,21 @@
 package kotlin_basic_w1
 
+fun main() {
+    var score : Int = 90
+
+    if(score >= 90) {
+        println("if : 합격(장학생)")
+    }
+    else if(score >= 60){
+        println("if : 합격")
+    }
+    else {
+        println("if : 불합격")
+    }
+
+    when(score) {
+        in 90..100 -> println("합격(장학생)")
+        in 60..89 -> println("합격")
+        else -> println("불합격")
+    }
+}
