30eeaa268bc99a3584025c9c402bb04e1ae0ffcf
Ahyoung Cho <joayoung03@gmail.com>
2022-12-29 23:57:08 +0900

    change

--- kotlin_basic_w1/exam01.kt
+++ kotlin_basic_w1/exam01.kt
@@ -1,10 +1,10 @@
 package kotlin_basic_w1
 
 fun main() {
-    var name : String = "슈먼이"
-    var id : Int = 20201231
-    var age : Int = 22
-    var dept : Char = 'S'
+    var name : String = "슈먼이" // var name = "슈먼이"
+    var id : Int = 20201231  // var id = 20201231
+    var age : Int = 22  // var age = 22
+    var dept : Char = 'S'  // var dept = 'S'
 
     println(name)
     println(id)
